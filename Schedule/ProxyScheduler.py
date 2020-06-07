0e38f1d0effb3713a2270f7d7a6e924476832a32
mkxzy <makina67@sina.com>
2020-06-07 18:17:39 +0800

    修改docker-compose

--- Schedule/ProxyScheduler.py
+++ Schedule/ProxyScheduler.py
@@ -51,7 +51,7 @@ def runScheduler():
     scheduler_log = LogHandler("scheduler_log")
     scheduler = BlockingScheduler(logger=scheduler_log)
 
-    scheduler.add_job(rawProxyScheduler, 'interval', minutes=5, id="raw_proxy_check", name="raw_proxy定时采集")
+    scheduler.add_job(rawProxyScheduler, 'interval', minutes=2, id="raw_proxy_check", name="raw_proxy定时采集")
     scheduler.add_job(usefulProxyScheduler, 'interval', minutes=1, id="useful_proxy_check", name="useful_proxy定时检查")
 
     scheduler.start()
